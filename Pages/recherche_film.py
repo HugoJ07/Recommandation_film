b36093f8143ed54c4816922d2487f321b1066f48
SemBesse_github <sembesse@hotmail.fr>
2025-06-05 12:28:35 +0200

    Ajout des colonne pour les images et amelioration pour la barre de recherche

--- Pages/recherche_film.py
+++ Pages/recherche_film.py
@@ -10,19 +10,21 @@ headers = {
         "accept": "application/json",
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGJlNWFjZDc1YmIzMWVlYTE2ZmMyY2VkMjU5YmM5ZiIsIm5iZiI6MTc0ODg1OTEyMy41MDQsInN1YiI6IjY4M2Q3OGYzMzVmOGU1MjAxODUzODM2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.06eO4o3IsGnUgXiyrinBOs0jUrexp-CLvKUjbJruAJY"
     }
-    st.title("Application de recommendation de film")
+st.title("Application de recommendation de film")
 
 df_machin = pd.read_csv("Data/title_sup_1950.csv")
 
 #st.write(df_machin)
 
-    selected = st.selectbox("Que voulez vous rechercher ?", ['Film', 'Acteur', 'Réalisateur'])
+selected = st.selectbox("Que voulez vous rechercher ?", ['Film', 'Acteur', 'Réalisateur'])
 
-    if selected == "Film":
-        search = st.text_input("Search movies by title", value="").lower()
-        titre = df_machin["originalTitle"].str.lower().str.contains(search)
-        df_title = df_machin[titre].reset_index()
 
+if selected == "Film":
+    df_machin["originalTitle"] = df_machin["originalTitle"].str.replace(r"[^\w\s]", "", regex=True)
+    search = st.text_input("Search movies by title", value="").lower()
+    titre = df_machin["originalTitle"].str.lower().str.contains(search)
+    df_title = df_machin[titre].reset_index()
+    
     if search:
 
         liste_id_imdb = df_title['tconst'].to_list()
@@ -40,14 +42,19 @@ df_machin = pd.read_csv("Data/title_sup_1950.csv")
                     dic_poster[id] = data["movie_results"][0]["poster_path"]
                 except:
                     pass
+        
+        
+        cols = st.columns(3)
 
-        for id_imdb, poster in dic_poster.items():
+        for idx, (id_imdb, poster) in enumerate(dic_poster.items()):
             if poster is not None:
-                st.image("https://image.tmdb.org/t/p/w500/" + poster)
+                col = cols[idx % 3]
+                with col:
+                    st.image("https://image.tmdb.org/t/p/w500/" + poster)
 
-                if st.button("Cliquer ici pour plus d'informations", key=id_imdb):    
-                    st.session_state['selected_film'] = id_imdb
-                    st.switch_page("pages/page_film.py")  
+                    if st.button("Cliquer ici pour plus d'informations", key=id_imdb):    
+                        st.session_state['selected_film'] = id_imdb
+                        st.switch_page("pages/page_film.py")  
 
 # elif selected == "Acteur": 
 #     search = st.text_input("Search movies by actor", value="")
