93eb9104d5819bb2e5c6552a2b39f28961fdac6d
SemBesse_github <sembesse@hotmail.fr>
2025-06-12 10:55:38 +0200

    sur les barres de recherche

--- Pages/recherche_film.py
+++ Pages/recherche_film.py
@@ -14,9 +14,8 @@ st.title("Application de recommendation de film")
 
 df_title = pd.read_csv("Data/title_sup_1950.csv")
 df_info = pd.read_csv("Data/info_film_leger.csv")
-#st.write(df_machin)
+#st.write(df_info)
 selected = st.selectbox("Que voulez vous rechercher ?", ['Film', 'Acteur', 'Réalisateur'])
-
 
 if selected == "Film":
     df_title["originalTitle"] = df_title["originalTitle"].str.replace(r"[^\w\s]", "", regex=True)
@@ -65,58 +64,39 @@ elif selected == "Acteur":
         if len(search2.split()) < 2:    
             st.warning("Veuillez entrer le **prénom et le nom** pour une recherche plus précise.")
         else:
-            actorName = df_info["primaryName"].str.lower().str.startswith(search2, na=False) & ((df_info["category"] == "actor") | (df_info["category"] == "actress"))
+            actorName = df_info["primaryName"].str.lower().str.contains(search2, na=False) & ((df_info["category"] == "actor") | (df_info["category"] == "actress"))
             result = df_info[actorName]
-                
-            list_id_actor = result["nconst"].tolist()
+            list_id_imdb = result["tconst"].tolist()
+            #st.write(list_id_imdb)
+            dic_poster = {}
+            for idx, id in enumerate(list_id_imdb):
+                if idx < 30 :
 
-            person_id = set()
-            for idx,id_imdb in enumerate(list_id_actor):
-                if idx < 5 :
+                    url = "https://api.themoviedb.org/3/find/"+ id +"?external_source=imdb_id"
+
+                    response = requests.get(url, headers=headers)
+
+                    data = json.loads(response.text)
+                    #st.write(data)
+
                     try:
-                        url = f"https://api.themoviedb.org/3/find/{id_imdb}?external_source=imdb_id"
-                        response = requests.get(url, headers=headers)
-                        data = response.json()
-                        person_id.add(data["person_results"][0]["id"])
+                        dic_poster[id] = data["movie_results"][0]["poster_path"]
                     except:
                         pass
 
-            list_poster = []
-            list_id_tmdb = []
-            list_imdb_id = []
-            #st.write(person_id)
-            #Ici je vais récupérer l'id imdb, le poster path et id tmdb du FILM ainsi que son posterPath. Je récupère l'id_tmdb pour récupérer l'id imdb du film pour la recherche 
-            for idx , id in enumerate(list(person_id)):
-                url_movies = "https://api.themoviedb.org/3/person/" + str(id) + "/movie_credits"
-                response2 = requests.get(url_movies, headers=headers)
-                data2 = json.loads(response2.text)
 
-                #st.write(data2)
-                for elem in data2["cast"]:
-                    poster = elem["poster_path"]
-                    id_tmdb = elem["id"]
-                    if poster and id_tmdb:
-                        list_poster.append(poster)
-                        list_id_tmdb.append(id_tmdb)
-                        url_id_imdb = f"https://api.themoviedb.org/3/movie/{id_tmdb}/external_ids"
-                        response_imdb = requests.get(url_id_imdb, headers=headers)
-                        data_imdb = json.loads(response_imdb.text)
-                        
-                        if data_imdb["imdb_id"] != None:
-                            list_imdb_id.append(data_imdb["imdb_id"])
+        cols = st.columns(3)
 
-            cols = st.columns(3)
+        for idx, (id_imdb, poster) in enumerate(dic_poster.items()):
+            if poster is not None:
+                col = cols[idx % 3]
+                with col:
+                    with st.container():
+                        st.image("https://image.tmdb.org/t/p/w500/" + poster)
 
-            for idx, (id_imdb, poster) in enumerate(zip(list_imdb_id, list_poster)):
-                if poster is not None:
-                    col = cols[idx % 3]
-                    with col:
-                        with st.container():
-                            st.image("https://image.tmdb.org/t/p/w500/" + poster)
-
-                            if st.button("Cliquer ici pour plus d'informations", key=id_imdb,use_container_width=True):    
-                                st.session_state['selected_film'] = id_imdb
-                                st.switch_page("pages/page_film.py")
+                        if st.button("Cliquer ici pour plus d'informations", key=id_imdb,use_container_width=True):    
+                            st.session_state['selected_film'] = id_imdb
+                            st.switch_page("pages/page_film.py")  
 
 else: 
     
@@ -128,57 +108,27 @@ else:
         directorName = df_info["primaryName"].str.lower().str.contains(search3, na=False) & (df_info["category"] == "director")
         
         result = df_info[directorName]
-        directorId = result["nconst"]
+        list_id_imdb = result["tconst"].tolist()
+        #st.write(list_id_imdb)
+        dic_poster = {}
+        for idx, id in enumerate(list_id_imdb):
+            if idx < 30 :
 
-
-        list_id_director = result["nconst"].to_list()
-        #st.write(result)
-        person_id = set()
-        #st.write(list_id_director)
-
-        for idx, id in enumerate(list_id_director):
-            #Grâce avec le nconst je viens récupérer son ID tmdb (a trois chiffre)
-            try:
                 url = "https://api.themoviedb.org/3/find/"+ id +"?external_source=imdb_id"
-                
+
                 response = requests.get(url, headers=headers)
 
                 data = json.loads(response.text)
-                #person_id = data["person_results"][0]["id"]
-                person_id.add(data["person_results"][0]["id"])
                 #st.write(data)
-                
 
-            except:
-                pass
-
-
-        list_poster = []
-        list_id_tmdb = []
-        list_imdb_id = []
-         #Ici je vais récupérer l'id imdb, le poster path et id tmdb du FILM ainsi que son posterPath. Je récupère l'id_tmdb pour récupérer l'id imdb du film pour la recherche 
-        for idx , id in enumerate(list(person_id)):
-            url_movies = "https://api.themoviedb.org/3/person/" + str(id) + "/movie_credits"
-            response2 = requests.get(url_movies, headers=headers)
-            data2 = json.loads(response2.text)
-            for elem in data2["crew"]:
-                if elem["job"] == "Director":
-                    poster = elem["poster_path"]
-                    id_tmdb = elem["id"]
-                    if poster and id_tmdb:
-                        list_poster.append(poster)
-                        list_id_tmdb.append(id_tmdb)
-                        url_id_imdb = f"https://api.themoviedb.org/3/movie/{id_tmdb}/external_ids"
-                        response_imdb = requests.get(url_id_imdb, headers=headers)
-                        data_imdb = json.loads(response_imdb.text)
-                    
-                        if data_imdb["imdb_id"] != None:
-                            list_imdb_id.append(data_imdb["imdb_id"])
-
+                try:
+                        dic_poster[id] = data["movie_results"][0]["poster_path"]
+                except:
+                    pass
 
         cols = st.columns(3)
 
-        for idx, (id_imdb, poster) in enumerate(zip(list_imdb_id, list_poster)):
+        for idx, (id_imdb, poster) in enumerate(dic_poster.items()):
             if poster is not None:
                 col = cols[idx % 3]
                 with col:
@@ -187,4 +137,4 @@ else:
 
                         if st.button("Cliquer ici pour plus d'informations", key=id_imdb,use_container_width=True):    
                             st.session_state['selected_film'] = id_imdb
-                            st.switch_page("pages/page_film.py")
+                            st.switch_page("pages/page_film.py")  
